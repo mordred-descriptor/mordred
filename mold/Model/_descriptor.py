11fd0ee121ca56735841e23c3feb21392d7e1892
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2016-01-04 08:01:50 +0900

    added Crippen

---
+++ mold/Model/_descriptor.py
@@ -0,0 +1,27 @@
+from .._base import Descriptor
+from rdkit.Chem import Crippen as _Crippen
+
+
+class Crippen(Descriptor):
+    descriptor_defaults = [('LogP',), ('MR',)]
+
+    def __init__(self, value='LogP'):
+        assert value in ['LogP', 'MR']
+        self.value = value
+
+    @property
+    def descriptor_name(self):
+        return 'Crippen{}'.format(self.value)
+
+    @property
+    def descriptor_key(self):
+        return self.make_key(self.value)
+
+    def calculate(self, mol):
+        if self.value == 'LogP':
+            return _Crippen.MolLogP(mol)
+        else:
+            return _Crippen.MolMR(mol)
+
+_descriptors = [Crippen]
+__all__ = [d.__name__ for d in _descriptors]
