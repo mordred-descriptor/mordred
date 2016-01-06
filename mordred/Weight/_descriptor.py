1eb604e284617771d9a5b79ada44b1231ee67666
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2016-01-06 19:48:47 +0900

    added Weight

---
+++ mordred/Weight/_descriptor.py
@@ -0,0 +1,30 @@
+from .._base import Descriptor
+from rdkit.Chem.Descriptors import ExactMolWt
+
+
+class Weight(Descriptor):
+    explicit_hydrogens = True
+
+    descriptor_defaults = [(False,), (True,)]
+
+    @property
+    def descriptor_name(self):
+        return 'AMW' if self.averaged else 'MW'
+
+    def __init__(self, averaged=False):
+        self.averaged = averaged
+
+    @property
+    def descriptor_key(self):
+        return self.make_key(self.averaged)
+
+    def calculate(self, mol):
+        w = ExactMolWt(mol)
+        if self.averaged:
+            w /= mol.GetNumAtoms()
+
+        return w
+
+
+_descriptors = [Weight]
+__all__ = [d.__name__ for d in _descriptors]
