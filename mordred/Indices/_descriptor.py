ab2c9cc56e8f8541d2f13d59cc6964997c604345
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2016-01-07 17:39:27 +0900

    add Zagreb index

--- mordred/Indices/_descriptor.py
+++ mordred/Indices/_descriptor.py
@@ -1,6 +1,7 @@
 from .._base import Descriptor
 from ._eccentric_connectivity import EccentricConnectivityIndex
+from ._zagreb import Zagreb
 
 
-_descriptors = [EccentricConnectivityIndex]
+_descriptors = [EccentricConnectivityIndex, Zagreb]
 __all__ = [d.__name__ for d in _descriptors]
---
+++ mordred/Indices/_zagreb.py
@@ -0,0 +1,45 @@
+from .._base import Descriptor
+from .._common import Valence
+
+
+class Zagreb(Descriptor):
+    explicit_hydrogens = False
+
+    descriptor_defaults = [(1, 1), (2, 1), (1, -1), (2, -1)]
+
+    @property
+    def dependencies(self):
+        return dict(
+            V=Valence.make_key(
+                self.explicit_hydrogens,
+                False,
+            ),
+        )
+
+    @property
+    def descriptor_name(self):
+        if self.variable in [1, -1]:
+            m = '' if self.variable == 1 else 'm'
+            return '{}Zagreb{}'.format(m, self.version)
+
+        return 'Zagreb{}_{}'.format(self.version, self.variable)
+
+    def __init__(self, version=1, variable=1):
+        assert version in [1, 2]
+        self.version = version
+        self.variable = variable
+
+    @property
+    def descriptor_key(self):
+        return self.make_key(self.version, self.variable)
+
+    def calculate(self, mol, V):
+        if not isinstance(self.variable, int) or self.variable < 0:
+            V = V.astype('float')
+
+        if self.version == 1:
+            return (V ** (self.variable * 2)).sum()
+        else:
+            return sum(
+                ((V[b.GetBeginAtomIdx()] * V[b.GetEndAtomIdx()]) ** self.variable
+                 for b in mol.GetBonds()))
