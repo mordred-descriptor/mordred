7ba9ca2565fc4659948a220e5ecea078f1e9439a
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2016-01-08 01:41:39 +0900

    add RotRatio

--- mordred/Bond/_descriptor.py
+++ mordred/Bond/_descriptor.py
@@ -1,11 +1,11 @@
 from ._bond_types import BondCount
 from ._hbond import HBondAcceptor, HBondDonor
-from ._rotatable import RotatableBondsCount
+from ._rotatable import RotatableBondsCount, RotatableBondsRatio
 
 _descriptors = [
     BondCount,
     HBondAcceptor, HBondDonor,
-    RotatableBondsCount
+    RotatableBondsCount, RotatableBondsRatio
 ]
 
 __all__ = [d.__name__ for d in _descriptors]
--- mordred/Bond/_rotatable.py
+++ mordred/Bond/_rotatable.py
@@ -1,4 +1,6 @@
 from .._base import Descriptor
+from ._bond_types import BondCount
+
 from rdkit.Chem.rdMolDescriptors import CalcNumRotatableBonds
 
 
@@ -15,3 +17,25 @@ class RotatableBondsCount(Descriptor):
 
     def calculate(self, mol):
         return CalcNumRotatableBonds(mol)
+
+
+class RotatableBondsRatio(Descriptor):
+    explicit_hydrogens = False
+
+    @property
+    def descriptor_name(self):
+        return 'RotRatio'
+
+    @property
+    def descriptor_key(self):
+        return self.make_key()
+
+    @property
+    def dependencies(self):
+        return dict(
+            nRot=RotatableBondsCount.make_key(),
+            nB=BondCount.make_key('O'),
+        )
+
+    def calculate(self, mol, nRot, nB):
+        return float(nRot) / float(nB)
