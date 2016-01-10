c25e52547f4c21189192999e0f72c57dfa919615
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2016-01-11 02:12:39 +0900

    add Fragment Complexity

---
+++ mordred/FragmentComplexity/_descriptor.py
@@ -0,0 +1,23 @@
+from .._base import Descriptor
+
+
+class FragmentComplexity(Descriptor):
+    explicit_hydrogens = False
+
+    @property
+    def descriptor_name(self):
+        return 'fragCpx'
+
+    @property
+    def descriptor_key(self):
+        return self.make_key()
+
+    def calculate(self, mol):
+        A = mol.GetNumAtoms()
+        B = mol.GetNumBonds()
+        H = sum((1 for a in mol.GetAtoms() if a.GetAtomicNum() != 6))
+        return abs(B ** 2 - A ** 2 + A) + float(H) / 100
+
+
+_descriptors = [FragmentComplexity]
+__all__ = [d.__name__ for d in _descriptors]
