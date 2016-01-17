26cf0e8ff8fc152fba0a6aa4ace10e33557bec7c
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2016-01-18 01:50:03 +0900

    fix test_parallel

--- tests/test_parallel.py
+++ tests/test_parallel.py
@@ -6,12 +6,13 @@ from numpy.testing import assert_almost_equal
 
 def test_parallel():
     calc = Calculator(all_descriptors())
-    mol = Chem.MolFromSmiles('C1=CC(=C(C=C1C2=C(C=C3C(=CC(=CC3=[O+]2)O)O)O)O)O')
+    mols = [
+        Chem.MolFromSmiles('c1ccccc1'),
+        Chem.MolFromSmiles('C1=CC(=C(C=C1C2=C(C=C3C(=CC(=CC3=[O+]2)O)O)O)O)O'),
+        Chem.MolFromSmiles('CCCCCC'),
+    ]
 
-    serial = calc(mol)
-    parallel = calc.map([mol])[0]
-
-    eq_([kv[0] for kv in serial], [kv[0] for kv in parallel])
-
-    for (_, s), (_, p) in zip(serial, parallel):
-        assert_almost_equal(s, p)
+    for serial, parallel in zip(map(calc, mols), calc.map(mols)):
+        for (d, s), (f, p) in zip(serial, parallel):
+            yield eq_, d, f
+            yield assert_almost_equal, s, p
