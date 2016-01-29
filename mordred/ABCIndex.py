eef391d31ba1a11c239481b8d5054095ea9dac7c
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2016-01-30 05:14:39 +0900

    add ABCIndex descriptor

---
+++ mordred/ABCIndex.py
@@ -0,0 +1,12 @@
+r"""
+References
+    * :cite:`furtula_atom-bond_2016`
+"""
+
+from ._abc_index import ABCIndex, ABCGGIndex
+
+__all__ = ('ABCIndex', 'ABCGGIndex',)
+
+if __name__ == '__main__':
+    from .__main__ import submodule
+    submodule()
---
+++ tests/test_ABCIndex.py
@@ -0,0 +1,24 @@
+from numpy.testing import assert_almost_equal
+
+from mordred.ABCIndex import ABCIndex, ABCGGIndex
+from rdkit import Chem
+
+# doi:10.2298/JSC150901093F
+data = """
+CC(C)CCCCCCC   6.58 6.49
+CCC(C)CCCCCC   6.47 6.58
+CC(C)(C)CCCCCC 6.84 6.82
+CCC(C)(C)CCCCC 6.68 6.95
+"""[1:-1]
+
+
+def test_ABC():
+    abc = ABCIndex()
+    abcgg = ABCGGIndex()
+
+    for line in data.split('\n'):
+        smi, dABC, dABCGG = line.strip().split()
+
+        mol = Chem.MolFromSmiles(smi)
+        yield assert_almost_equal, abc(mol), float(dABC), 2
+        yield assert_almost_equal, abcgg(mol), float(dABCGG), 2
