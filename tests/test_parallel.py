e01972a674d611ad54f27f00e3117af2e9cb5a0c
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2016-02-05 04:52:16 +0900

    relax test_parallel(fall precision of Conformer when ToBinary)

--- tests/test_parallel.py
+++ tests/test_parallel.py
@@ -6,11 +6,11 @@ from numpy.testing import assert_almost_equal
 
 def test_parallel():
     calc = Calculator(all_descriptors())
-    mols = [
+    mols = list(map(Chem.AddHs, [
         Chem.MolFromSmiles('c1ccccc1'),
         Chem.MolFromSmiles('C1=CC(=C(C=C1C2=C(C=C3C(=CC(=CC3=[O+]2)O)O)O)O)O'),
         Chem.MolFromSmiles('CCCCCC'),
-    ]
+    ]))
 
     for mol in mols:
         Chem.EmbedMolecule(mol)
@@ -18,4 +18,4 @@ def test_parallel():
     for (_, serial), (_, parallel) in zip(calc.map(mols, processes=1), calc.map(mols)):
         for (d, s), (f, p) in zip(serial, parallel):
             yield eq_, d, f
-            yield assert_almost_equal, s, p
+            yield assert_almost_equal, s, p, 3, str(d)
