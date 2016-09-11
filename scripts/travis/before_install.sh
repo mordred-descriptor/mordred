ee3241e517809dbe9cc3b8bb54e5492c814f07a6
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2016-09-12 02:04:51 +0900

    cleanup files

--- scripts/travis/before_install.sh
+++ scripts/travis/before_install.sh
@@ -5,7 +5,7 @@ set -e
 eval "$(ssh-agent -s)"
 ssh-add -D
 
-openssl aes-256-cbc -K $encrypted_8d665ddaa7bd_key -iv $encrypted_8d665ddaa7bd_iv -in .id_rsa.enc -out ~/.ssh/.id_rsa -d
+openssl aes-256-cbc -K $encrypted_8d665ddaa7bd_key -iv $encrypted_8d665ddaa7bd_iv -in ./scripts/id_rsa.enc -out ~/.ssh/.id_rsa -d
 chmod 600 ~/.ssh/.id_rsa
 ssh-add ~/.ssh/.id_rsa
 
