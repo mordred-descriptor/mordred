e79d464957f23bbbe7c480d3d303d55f3b3b224b
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2017-09-07 10:14:14 +0900

    add tests

--- mordred/_base/util.py
+++ mordred/_base/util.py
@@ -4,6 +4,14 @@ from ..error import MissingValueBase
 def is_missing(v):
     """Check argument is either MissingValue or not.
 
+    >>> from mordred.error import Missing, Error
+    >>> is_missing(1)
+    False
+    >>> is_missing(Missing(ValueError("missing"), ()))
+    True
+    >>> is_missing(Error(ValueError("error"), ()))
+    True
+
     Parameters:
         v(any): value
 
