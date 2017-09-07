51fad0f1ea576acf45bc76304b9ac3ad8024d43f
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2017-09-07 10:31:15 +0900

    fix example

--- examples/020-single_mol-multiple_desc.py
+++ examples/020-single_mol-multiple_desc.py
@@ -4,55 +4,51 @@ from rdkit import Chem
 
 from mordred import Chi, ABCIndex, RingCount, Calculator, is_missing, descriptors
 
-freeze_support()
+if __name__ == "__main__":
+    freeze_support()
 
-benzene = Chem.MolFromSmiles("c1ccccc1")
+    benzene = Chem.MolFromSmiles("c1ccccc1")
 
-# Create empty Calculator instance
-calc1 = Calculator()
+    # Create empty Calculator instance
+    calc1 = Calculator()
 
-# Register descriptor instance
-calc1.register(Chi.Chi(type="path_cluster", order=4))
+    # Register descriptor instance
+    calc1.register(Chi.Chi(type="path_cluster", order=4))
 
-# Register descriptor class using preset
-calc1.register(RingCount.RingCount)
+    # Register descriptor class using preset
+    calc1.register(RingCount.RingCount)
 
-# Register all descriptors in module
-calc1.register(ABCIndex)
+    # Register all descriptors in module
+    calc1.register(ABCIndex)
 
+    # Calculate descriptors
+    result = calc1(benzene)
 
-# Calculate descriptors
-result = calc1(benzene)
+    print(result)
+    # >>> [0.0, 1, 0, 0, 0, 1, (snip)
 
-print(result)
-# >>> [0.0, 1, 0, 0, 0, 1, (snip)
+    # Calculator constructor can register descriptors
+    calc2 = Calculator(Chi.Chi)
 
+    # Descriptors module contains all descriptors
+    calc3 = Calculator(descriptors)
 
-# Calculator constructor can register descriptors
-calc2 = Calculator(Chi.Chi)
+    # User can access all descriptor instances by descriptors property
+    print(calc3.descriptors)
+    # >>> (mordred.EccentricConnectivityIndex.EccentricConnectivityIndex(), (snip)
 
-# Descriptors module contains all descriptors
-calc3 = Calculator(descriptors)
+    # Calculate descriptors
+    result = calc3(benzene)
 
-# User can access all descriptor instances by descriptors property
-print(calc3.descriptors)
-# >>> (mordred.EccentricConnectivityIndex.EccentricConnectivityIndex(), (snip)
+    # get first missing value
+    na1 = next(r for r in result if is_missing(r))
 
+    # get reason
+    print(na1.error)
+    # >>> missing 3D coordinate
 
-# Calculate descriptors
-result = calc3(benzene)
+    # Delete all missing value
+    result = result.drop_missing()
 
-# get first missing value
-na1 = next(r for r in result if is_missing(r))
-
-# get reason
-print(na1.error)
-# >>> missing 3D coordinate
-
-
-# Delete all missing value
-result = result.drop_missing()
-
-
-# convert to dict
-print(result.asdict())
+    # convert to dict
+    print(result.asdict())
--- examples/030-multiple_mol-multiple_desc.py
+++ examples/030-multiple_mol-multiple_desc.py
@@ -4,19 +4,20 @@ from rdkit import Chem
 
 from mordred import Calculator, descriptors
 
-freeze_support()
+if __name__ == "__main__":
+    freeze_support()
 
-mols = [
-    Chem.MolFromSmiles("c1ccccc1"),
-    Chem.MolFromSmiles("c1ccccc1Cl"),
-    Chem.MolFromSmiles("c1ccccc1C"),
-]
+    mols = [
+        Chem.MolFromSmiles("c1ccccc1"),
+        Chem.MolFromSmiles("c1ccccc1Cl"),
+        Chem.MolFromSmiles("c1ccccc1C"),
+    ]
 
-# Create Calculator
-calc = Calculator(descriptors)
+    # Create Calculator
+    calc = Calculator(descriptors)
 
-# map method calculate multiple molecules (return generator)
-print(list(calc.map(mols)))
+    # map method calculate multiple molecules (return generator)
+    print(list(calc.map(mols)))
 
-# pandas method calculate multiple molecules (return pandas DataFrame)
-print(calc.pandas(mols))
+    # pandas method calculate multiple molecules (return pandas DataFrame)
+    print(calc.pandas(mols))
