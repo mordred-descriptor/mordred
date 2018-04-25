787fdc2279f0f713e16d8b5ec777ffcdfc197211
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2018-04-25 19:54:07 +0900

    fix example

--- examples/article/Code4.py
+++ examples/article/Code4.py
@@ -1,5 +1,7 @@
+from distutils.version import StrictVersion
+
 from mordred.RingCount import RingCount
 
 # Start Code 4
-presets = list(RingCount.preset())
+presets = list(RingCount.preset(version=StrictVersion("1.0.0")))
 print(len(presets))
