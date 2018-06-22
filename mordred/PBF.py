9adab96fde1d935354fb2b722c8ec68b1ebf9990
Whisperwind22 <BmhPkixggjpMzjVF9Ceq>
2018-06-22 18:39:49 +0300

    PBF rdkit

---
+++ mordred/PBF.py
@@ -0,0 +1,32 @@
+from rdkit.Chem.AllChem import CalcPBF
+
+from ._base import Descriptor
+
+__all__ = (
+    "PBF",
+)
+
+
+class PBF(Descriptor):
+    r"""PBF descriptor.
+    """
+    __slots__ = ()
+    since = "1.1.0"
+
+    @classmethod
+    def preset(cls):
+        yield cls()
+
+    def description(self):
+        return self.__class__.__name__
+
+    def __str__(self):
+        return self.__class__.__name__
+
+    def parameters(self):
+        return ()
+
+    def calculate(self):
+        return CalcPBF(self.mol)
+
+    rtype = float
