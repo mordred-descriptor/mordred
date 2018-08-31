f8ceddf5158c30472ff6897b7d0665043d7f3c92
Vladimir Moskalev <moskalev@insilicomedicine.com>
2018-08-31 14:05:17 +0300

    QED rdkit

---
+++ mordred/QED.py
@@ -0,0 +1,32 @@
+from rdkit.Chem.QED import qed
+
+from ._base import Descriptor
+
+__all__ = (
+    "QED",
+)
+
+
+class QED(Descriptor):
+    r"""QED descriptor."""
+
+    __slots__ = ()
+    since = "1.1.2"
+
+    @classmethod
+    def preset(cls, version):
+        yield cls()
+
+    def description(self):
+        return self.__class__.__name__
+
+    def __str__(self):
+        return self.__class__.__name__
+
+    def parameters(self):
+        return ()
+
+    def calculate(self):
+        return qed(self.mol)
+
+    rtype = float
