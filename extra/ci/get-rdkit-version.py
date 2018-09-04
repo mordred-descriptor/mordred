6b85ed417b0b32f6d71727e61c84aa012ec391a6
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2018-09-04 17:29:52 +0900

    downgrade rdkit on windows/python3.5

--- extra/ci/get-rdkit-version.py
+++ extra/ci/get-rdkit-version.py
@@ -1,8 +1,12 @@
+import re
+
+comment = re.compile(r'^\s*#')
+
 def main(data, os, pyver):
     d = {
         (os, pyver): ver
         for os, pyver, ver
-        in (line.strip().split() for line in open(data))
+        in (line.strip().split() for line in open(data) if not comment.match(line))
     }
 
     print(d[(os, pyver)])
