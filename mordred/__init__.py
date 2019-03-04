db700139e69e5de9f828dfd918e6b7d8b5cec43b
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2019-03-04 21:39:26 +0900

    isort

--- mordred/__init__.py
+++ mordred/__init__.py
@@ -1,14 +1,9 @@
 r"""modred descriptor calculator."""
 
 from ._base import (
-    Calculator,
-    Descriptor,
-    get_descriptors_from_module,
-    get_descriptors_in_module,
-    is_missing,
-    Result,
+    Result, Calculator, Descriptor, is_missing, get_descriptors_in_module,
+    get_descriptors_from_module
 )
-
 from ._version import __version__
 
 __all__ = (
